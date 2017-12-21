f1ea02e30e773ae4cc35a6ba91c0c0e520f60e7f
Haseeb Mohammed <>
2017-12-20 20:57:52 -0600

    Update environment.ts

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,22 +1,22 @@
 export const environment = {
     production: false,
 
-    region: 'us-east-2',
+    region: 'us-east-1',
 
-    identityPoolId: 'us-east-2:c2a5e794-a820-488e-b4cd-87b24c399f67',
-    userPoolId: 'us-east-2_pfLWG9lTe',
-    clientId: '57r1n4i3gu9ecbjeoqr3ulc046',
+    identityPoolId: 'us-east-1:fbe0340f-9ffc-4449-a935-bb6a6661fd53',
+    userPoolId: 'us-east-1_PGSbCVZ7S',
+    clientId: 'hh5ibv67so0qukt55c5ulaltk',
 
     rekognitionBucket: 'rekognition-pics',
     albumName: "usercontent",
-    bucketRegion: 'us-east-2',
+    bucketRegion: 'us-east-1',
 
-    ddbTableName: 'LoginTrailprayertimes',
-    prayerTimes: 'prayerTimes',
+    ddbTableName: 'LoginTrail',
+
     cognito_idp_endpoint: '',
     cognito_identity_endpoint: '',
     sts_endpoint: '',
     dynamodb_endpoint: '',
     s3_endpoint: ''
-};
 
+};
